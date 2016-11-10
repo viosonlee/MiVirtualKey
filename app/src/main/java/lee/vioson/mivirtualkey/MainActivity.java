96cd075798eb1b2256cb16562a79d4cd393ac74f
viosonlee <1239251224@qq.com>
2016-11-10 14:59:09 +0800

    Initial commit

---
+++ app/src/main/java/lee/vioson/mivirtualkey/MainActivity.java
@@ -0,0 +1,13 @@
+package lee.vioson.mivirtualkey;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
